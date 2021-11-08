b2a626a1db9b436b39031752bb9af7491d23a50f
Arek <arrek1116@wp.pl>
2021-11-08 23:58:59 +0100

    Server push

--- server/app.js
+++ server/app.js
@@ -1,7 +1,22 @@
-var express = require('express');
-var cors = require('cors')
+const express = require('express');
+const cors = require('cors')
+const router = express.Router()
+const app = express();
 
-var app = express();
+
+// DB + check connection with DB
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db){
+  if(err){
+    throw err;
+  } else {
+    console.log("Successful connection to MongoDB")
+    db.close()
+  }
+})
+
 
 app.use(cors({
   origin: "*"
@@ -12,28 +27,94 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-app.post('/login', function(req, res) {
-  console.log("\n")
-  console.log(req.body.login)
-  console.log(req.body.password)
-  console.log("Post at /login")
+
+
+
+app.use("/", router);
+
+router.route("/login").post(function(req,res) {
+  var user = {
+  login:req.body.login,
+  password:req.body.password
+  }
+  var newNumOfLogins = 0
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("helloMernBase");
+    dbo.collection("users").findOne(user, function(err,result) {
+      if(err){
+        throw err;
+      } else {
+        console.log(result)
+        newNumOfLogins = result.numOfLogins + 1;
+      }
+      var newValue = {$set : {numOfLogins:newNumOfLogins}}
+      dbo.collection("users").updateOne(user, newValue, function(err,res) {
+        if(err) {
+          throw err;
+        } else {
+          console.log("Updated");
+          db.close();
+        }
+      })
+    })
+    });
 });
 
-app.post('/register', function(req, res) {
-  console.log("\n")
-  console.log(req.body.email)
-  console.log(req.body.login)
-  console.log(req.body.password)
-  console.log("Register at /register")
-});
 
-app.get('/getusers', function(req, res) {
-  console.log("Getusers at /getusers")
-});
+router.route("/register").post(function(req,res) {
+  var toInsert = {
+    email:req.body.email,
+    login:req.body.login,
+    password:req.body.password,
+    numOfLogins:0}
+  MongoClient.connect(url, function(err,db) {
+    if(err){
+      throw err;
+    } else {
+      var dbo = db.db("helloMernBase");
+      dbo.collection("users").insertOne(toInsert,function(err,res) {
+        if(err){
+          throw err;
+        } else {
+          console.log("Register successful")
+          db.close()
+        }
+      })
+    }
+  })
+})
 
-app.delete('/delete', function(req, res) {
-  console.log("Delete at /delete")
-});
+
+router.route("/delete").post(function(req,res) {
+  var toDelete = {login:req.body.login}
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("helloMernBase");
+    dbo.collection("users").deleteOne(toDelete,function(err,res) {
+      if(err){
+        throw err;
+      } else {
+        console.log("Removal successful")
+        db.close()
+      }
+    })
+  });
+})
+
+
+router.route("/getUsers").get(function(req,res) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("helloMernBase");
+    dbo.collection("users").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      res.json(result)
+      db.close()
+    });
+  });
+})
+
 
 app.listen(3001, function() {
   console.log('Listening');
